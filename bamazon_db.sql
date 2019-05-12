2656fa87fc3f26fb9ac1934899b1be6b29f4cecf
moises eastland <dewaynepade95@gmail.com>
2019-05-12 16:03:15 -0500

    added tables and inserts

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -8,4 +8,36 @@ product_name VARCHAR(15),
 department_name VARCHAR(15),
 price INTEGER(6),
 stock_quantity INTEGER(5) AUTO_INCREMENT 
-)
+);
+
+
+INSERT INTO products(product_name,department_name,price,)
+VALUES("COFFEE TABLE","FUNITURE",200);
+
+INSERT INTO products(product_name,department_name,price,)
+VALUES("bottled water","drinks",8);
+
+INSERT INTO products(product_name,department_name,price)
+VALUES("");
+
+INSERT INTO products(product_name,department_name,price)
+VALUES("");
+
+INSERT INTO products(product_name,department_name,price)
+VALUES("");
+
+INSERT INTO product(product_name,department_name,price)
+VALUES("");
+
+INSERT INTO products(product_name,department_name,price)
+VALUES("");
+
+INSERT INTO products(product_name,department_name,price)
+VALUES("");
+
+INSERT INTO products(product_name,department_name,price)
+VALUES("");
+
+INSERT INTO products(product_name,department_name,price)
+VALUES("");
+
