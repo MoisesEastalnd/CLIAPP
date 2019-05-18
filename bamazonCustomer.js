74cd194204e6d36ea76f58b92f97c5356d526321
moises eastland <dewaynepade95@gmail.com>
2019-05-17 22:15:30 -0500

    updating customer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,59 @@
 var mysql = require("mysql");
+function displayProduct(){
+    inquirer 
+    .prompt([
+        {
+            id:"",
+            name:"",
+            price:""
+        },{
+            id:"",
+            name:"",
+            price:""
+        },{
+            id:"",
+            name:"",
+            price:""
+        },{
+            id:"",
+            name:"",
+            price:""
+        },{
+            id:"",
+            name:"",
+            price:""
+        },{
+            id:"",
+            name:"",
+            price:""
+        },{
+            id:"",
+            name:"",
+            price:""
+        },{
+            id:"",
+            name:"",
+            price:""
+        },{
+            id:"",
+            name:"",
+            price:""
+        },{
+            id:"",
+            name:"",
+            price:""
+        },
+        {
+            message:"What is the ID number of the item you would like to purchase?",
+            message:"How many units would like to purchase?",
+        }
+           function invertory(){
+               if displayProduct < 0 {
+                   return quantity -1
+               }
+           }
+    
+       
+    ])
+}
 
